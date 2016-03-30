2bcac70b4f172bebdbe45fa27c3b22eee140cabd
Ivan Zhuravlev <ivanszhuravlev@gmail.com>
2016-03-30 11:28:29 +0300

    beginning

---
+++ Stalins_notebook/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Stalins_notebook.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("Contacts")
+        { }
+        public DbSet<ContactsModel> Contacts { get; set; }
+
+    }
+}
---
+++ Stalins_notebook/scripts/index.js
@@ -0,0 +1,17 @@
+﻿function form_open() {
+    document.getElementById("layer_1").classList.add("closed");
+}
+
+function enter_activate() {
+    document.getElementById("reg_tab").classList.remove("active");
+    document.getElementById("enter_tab").classList.add("active");
+    document.getElementById("register").classList.add("disabled");
+    document.getElementById("enter").classList.remove("disabled");
+}
+
+function reg_activate() {
+    document.getElementById("enter_tab").classList.remove("active");
+    document.getElementById("reg_tab").classList.add("active");
+    document.getElementById("enter").classList.add("disabled");
+    document.getElementById("register").classList.remove("disabled");
+}
